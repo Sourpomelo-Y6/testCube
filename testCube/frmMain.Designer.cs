7a1cdd4f7a969c6fd53e25a171c0a48a769183d9
Sourpomelo-Y6 <hi-y6@hotmail.co.jp>
2020-09-30 17:29:38 +0900

    プロジェクト ファイルを追加します。

---
+++ testCube/frmMain.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace testCube
+{
+    partial class frmMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlMain = new System.Windows.Forms.Panel();
+            this.pbMain = new System.Windows.Forms.PictureBox();
+            this.txtDebug = new System.Windows.Forms.TextBox();
+            this.pnlMain.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMain)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlMain
+            // 
+            this.pnlMain.Controls.Add(this.pbMain);
+            this.pnlMain.Location = new System.Drawing.Point(12, 12);
+            this.pnlMain.Name = "pnlMain";
+            this.pnlMain.Size = new System.Drawing.Size(200, 200);
+            this.pnlMain.TabIndex = 0;
+            // 
+            // pbMain
+            // 
+            this.pbMain.BackColor = System.Drawing.Color.White;
+            this.pbMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pbMain.Location = new System.Drawing.Point(0, 0);
+            this.pbMain.Name = "pbMain";
+            this.pbMain.Size = new System.Drawing.Size(200, 200);
+            this.pbMain.TabIndex = 0;
+            this.pbMain.TabStop = false;
+            // 
+            // txtDebug
+            // 
+            this.txtDebug.Location = new System.Drawing.Point(290, 19);
+            this.txtDebug.Multiline = true;
+            this.txtDebug.Name = "txtDebug";
+            this.txtDebug.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtDebug.Size = new System.Drawing.Size(200, 230);
+            this.txtDebug.TabIndex = 1;
+            this.txtDebug.WordWrap = false;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtDebug);
+            this.Controls.Add(this.pnlMain);
+            this.Name = "frmMain";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.pnlMain.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbMain)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlMain;
+        private System.Windows.Forms.PictureBox pbMain;
+        private System.Windows.Forms.TextBox txtDebug;
+    }
+}
+
---
+++ testCube/frmMain.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace testCube
+{
+    public partial class frmMain : Form
+    {
+        class PointRPY 
+        {
+            /// <summary>Rotate Roll Radian </summary>
+            public double Roll;
+
+            /// <summary>Rotate Pitch Radian </summary>
+            public double Pitch;
+
+            /// <summary>Rotate Yaw Radian </summary>
+            public double Yaw;
+
+
+            public double x;
+            public double y;
+            public double z;
+
+
+            public PointRPY(double degRoll, double degPitch , double degYaw)
+            {
+                this.Roll = Deg2Rad(degRoll);
+                this.Pitch = Deg2Rad(degPitch);
+                this.Yaw = Deg2Rad(degYaw);
+
+                this.x = 100 * Math.Sqrt(3) * Math.Cos(Math.PI / 2.0 - Pitch) * Math.Cos(Yaw);
+                this.y = 100 * Math.Sqrt(3) * Math.Cos(Math.PI / 2.0 - Pitch) * Math.Sin(Yaw);
+                this.z = 100 * Math.Sqrt(3) * Math.Sin(Math.PI / 2.0 - Pitch) ;
+            }
+
+            private double Deg2Rad(double deg)
+            {
+                return Math.PI * deg / 180.0;
+            }
+        };
+
+
+        PointRPY[] pCube;
+
+
+
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            double DistanceCamera = 30;
+            double DistanceScreen = 10;
+            double DiceSize = 100;
+
+            double pitch_wdeg = 180 * Math.Acos(1.0 / Math.Sqrt(3)) / Math.PI;
+
+            pCube = new PointRPY[8] {
+                new PointRPY(0, pitch_wdeg,  45),
+                new PointRPY(0,180-pitch_wdeg,  45),
+                new PointRPY(0, pitch_wdeg, -45),
+                new PointRPY(0, pitch_wdeg, 135),
+                new PointRPY(0,180-pitch_wdeg, -45),
+                new PointRPY(0,180-pitch_wdeg, 135),
+                new PointRPY(0, pitch_wdeg, 225),
+                new PointRPY(0,180-pitch_wdeg, 225),
+            };
+
+            Bitmap bmp = new Bitmap(pbMain.Width, pbMain.Height);
+            Graphics g = Graphics.FromImage(bmp);
+
+            g.FillRectangle(Brushes.Red, 0, 0, pbMain.Width, pbMain.Height);
+
+
+            foreach (var p in pCube) 
+            {
+                //txtDebug.AppendText(p.x.ToString() + "," + p.y.ToString() + "," + p.z.ToString() + Environment.NewLine);
+
+                double workx = (p.x * DistanceCamera) / (p.z + DistanceCamera + DistanceScreen);
+                double worky = (p.y * DistanceCamera) / (p.z + DistanceCamera + DistanceScreen);
+
+                txtDebug.AppendText(workx.ToString()+","+worky.ToString()+Environment.NewLine);
+                g.DrawRectangle(Pens.Black, (float)workx+100, (float)worky+100, 2, 2);
+            }
+
+
+            if (pbMain.Image != null) 
+            {
+                pbMain.Image.Dispose();
+                pbMain.Image = null;
+            }
+            pbMain.Image = (Image)bmp.Clone();
+
+            g.Dispose();
+            bmp.Dispose();
+        }
+    }
+}
